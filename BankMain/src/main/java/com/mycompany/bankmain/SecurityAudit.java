02bbec9a015f292b13b4a75b01e6d1aeaefc81cc
108hills <hiubangkai1@gmail.com>
2026-03-06 15:32:03 +0700

    update sec audit

--- BankMain/src/main/java/com/mycompany/bankmain/SecurityAudit.java
+++ BankMain/src/main/java/com/mycompany/bankmain/SecurityAudit.java
@@ -16,5 +16,22 @@ public class SecurityAudit {
         this.suspiciousActivity = false;
     }
     
-    public void auditTransaction
+    public void auditTransaction(double amount) {
+        if (amount > this.maxTransactionLimit) {
+            this.suspiciousActivity = true;
+            System.out.println("ALERT: Transaksi" + amount + " melebihi max balance dari" + this.maxTransactionLimit + ".");
+            System.out.println("ACTION: Akun di flag untuk manual review.");
+        } else {
+            System.out.println("AUDIT: Nominal transaksi aman.");
+        }
+    }
+    
+    public boolean isAccountFlagged() {
+        return this.suspiciousActivity;
+    }
+    
+    public void resetFlag() {
+        this.suspiciousActivity = false;
+        System.out.println("SYSTEM: Security flag dibersihkan.");
+    }
 }
