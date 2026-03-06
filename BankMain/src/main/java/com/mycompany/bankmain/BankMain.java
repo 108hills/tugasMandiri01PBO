bb822c6caae2a9490bc099ecdcb80eb37a589ff2
mrongn <hiubangkai1@gmail.com>
2026-03-06 14:55:00 +0700

    update main

--- BankMain/src/main/java/com/mycompany/bankmain/BankMain.java
+++ BankMain/src/main/java/com/mycompany/bankmain/BankMain.java
@@ -5,12 +5,53 @@
 package com.mycompany.bankmain;
 
 /**
- *
- * @author hiuba
+ * @author imrong
  */
+
 public class BankMain {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        private String accountNumber;
+        private double balance;
+        
+        public BankAccount(String accountNum, double initBalance) {
+            this.accountNumber = accountNum;
+            
+            if (initBalance >= 0 ) {
+                this.balance = initBalance;
+            } else {
+                this.balance = 0.0;
+                System.out.println("Balance berada pada negatif/kosong, default 0.");
+            }
+        }
+        
+        public String getAccNumber() {
+            return this.accountNumber;
+        }
+        
+        public double getBal() {
+            return this.balance;
+        }
+        
+        public void deposit(double amount) {
+            if (amount > 0) {
+                balance += amount;
+                System.out.println("Berhasil. Deposit:" + amount);
+            } else {
+                System.out.println("Deposit gagal. Nominal harus lebih dari 0.");
+            }
+        }
+        
+        public void withdraw(double amount) {
+            if (amount <= 0) {
+                System.out.println("Withdraw gagal. Nominal harus lebih dari 0.");
+            } else if (amount > this.balance) {
+                System.out.println("Withdraw gagal. Nominal harus kurang dari/sama dengan Balance. Balance:" + this.balance);
+            } else {
+                this.balance -= amount;
+                System.out.println("Withdraw" + amount + " berhasil.");
+            }
+        }
     }
 }
+
--- BankMain/src/main/java/com/mycompany/bankmain/Customer.java
+++ BankMain/src/main/java/com/mycompany/bankmain/Customer.java
@@ -5,9 +5,33 @@
 package com.mycompany.bankmain;
 
 /**
- *
- * @author hiuba
+ * @author imrong
  */
 public class Customer {
+    private String customerID;
+    private String fullName;
+    private String email;
     
+    public Customer(String customerID, String fullName) {
+        this.customerID = customerID;
+        this.fullName = fullName;
+        this.email = "Belum ada";
+    }
+    
+    public void setEmail(String email) {
+        if (email != null && email.contains("@") && email.contains(".")) {
+            this.email = email;
+            System.out.println("Email berhasil diupdate.");
+        } else {
+            System.out.println("Error: format salah.");
+        }
+    }
+    
+    public String getEmail() {
+        return this.email;
+    }
+    
+    public String displayProfile() {
+        System.out.println("Customer ID: " + this.customerID + " | Nama: " + this.fullName + " | Email: " + this.email);
+    }
 }
